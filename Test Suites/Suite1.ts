bea1ba82b746ba99e0919071c5623d808f950ec8
thalesh87 <38219342+thalesh87@users.noreply.github.com>
2019-12-08 17:54:27 +0530

    uploaded

---
+++ Test Suites/Suite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7ce18470-e2ea-421c-a42c-d3ee80884b79</testSuiteGuid>
+   <testCaseLink>
+      <guid>679608f7-d4fe-4cf3-b59a-991f56342464</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
